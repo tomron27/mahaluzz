972d631b857a193cd7546b278964d19e56fa4dd9
tomron27 <tomron27@gmail.com>
2018-12-19 19:45:07 +0200

    Added user authentication

---
+++ main/user_auth.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+
+def check_user(username, password):
+    user_queryset = User.objects.filter(username=username)
+    if not user_queryset.exists():
+        return True, "משתמש לא קיים"
+
+    if not user_queryset[0].check_password(password):
+        return True, "סיסמה שגויה"
+
+    return False, "הצלחה"
+
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from .user_auth import check_user
 from django.http import HttpResponse
 from django.utils.translation import ugettext_lazy as _
 
@@ -7,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 
 def login(request):
     # if this is a POST request we need to process the form data
+    has_errors = message = False
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = LoginForm(request.POST)
@@ -14,13 +16,16 @@ def login(request):
         password = request.POST['password']
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return render(request, 'home.html')
+            has_errors, message = check_user(username, password)
+            print(has_errors, message)
+            if has_errors:
+                return render(request, 'registration/login.html',
+                                {'form': form, 'has_errors': has_errors, 'message': message})
+            else:
+                return render(request, 'home.html')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = LoginForm()
 
-    return render(request, 'registration/login.html', {'form': form})
+    return render(request, 'registration/login.html', {'form': form, 'has_errors': has_errors, 'message': message})
