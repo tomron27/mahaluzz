4acc4c328a054cdfe0e57796d2f31912aef7158a
Doron Butnaru <doronbut@gmail.com>
2018-12-27 11:24:04 +0200

    USERS_GROUP

--- main/user_auth.py
+++ main/user_auth.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 def check_user(username, password):
     user_queryset = User.objects.filter(username=username)
     if not user_queryset.exists():
-        return True, "משתמש לא קיים"
+        return True, "משתמש לא קיים",None
 
     if not user_queryset[0].check_password(password):
-        return True, "סיסמה שגויה"
+        return True, "סיסמה שגויה",user_queryset
 
-    return False, "הצלחה"
+    return False, "הצלחה",user_queryset
 
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import LoginForm
 from .user_auth import check_user
+from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
 from django.utils.translation import ugettext_lazy as _
 
@@ -14,11 +15,25 @@ def login(request):
         form = LoginForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            has_errors, message = check_user(form.cleaned_data['username'], form.cleaned_data['password'])
+            has_errors, message,user_queryset = check_user(form.cleaned_data['username'], form.cleaned_data['password'])
             if has_errors:
+                # Error - Login Again
                 return render(request, 'registration/login.html',
                                 {'form': form, 'has_errors': has_errors, 'message': message})
             else:
+                #TODO: Jump to page by User Group
+
+                uid = user_queryset.values_list('id')
+                #print(uid)
+                #print(uid[0][0])
+                user = User.objects.get(id=uid[0][0])
+                print(user)
+                print(user.groups.all()[0])
+                user_Group = user.groups.all()[0]
+                if (user_Group == 'Parents'):
+                    return render(request, 'home.html')
+                if (user_Group == 'Master'):
+                    return render(request, 'home.html')
                 return render(request, 'home.html')
 
     # if a GET (or any other method) we'll create a blank form
