c7758700eac056dc7c2f888cf259ebeac76ea471
Noga <tenenoga@gmail.com>
2018-12-27 17:52:27 +0200

    Merge branch 'master' of https://github.com/tomron27/mahaluzz
    # Conflicts:
    #	.idea/mahaluzz.iml
    #	.idea/misc.xml

--- main/models.py
+++ main/models.py
@@ -1,25 +1,11 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User, Group
 
-
-class Student(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    parent1 = models.CharField(max_length=30)
-    parent2 = models.CharField(max_length=30, blank=True)
-    classroom = models.CharField(max_length=5)
-    birthday = models.DateField()
-
-    def create(self):
-        self.save()
-
-    def __str__(self):
-        return self.id
 
 class Classroom(models.Model):
     name = models.CharField(max_length=5, primary_key=True)
-    teacher = models.CharField(max_length=30)
+    teacher = models.Foreignkey(User)
 
     def create(self):
         self.save()
@@ -28,21 +14,41 @@ class Classroom(models.Model):
         return self.name
 
 
-class Schedule(models.Model):
-    #day_of_week = models.PositiveIntegerField(primary_key=True)
-    #hour = models.PositiveIntegerField(primary_key=True)
-    #classroom= models.CharField(max_length=5, primary_key=True)
-    # subject= ************
+class Student(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    parent1 = models.Foreignkey(User)
+    parent2 = models.Foreignkey(User)
+    classroom = models.Foreignkey(Classroom)
+    birthday = models.DateField()
 
     def create(self):
         self.save()
 
+    def __str__(self):
+        return self.id
+
+
+class Schedule(models.Model):
+    schedule_id = models.CharField(max_length=10, primary_key=True)
+    day_of_week = models.PositiveIntegerField()
+    hour = models.PositiveIntegerField()
+    classroom = models.Foreignkey(Classroom)
+    teacher = models.Foreignkey(User)
+    subject = models.CharField(max_length=30)
+
+    def create(self):
+        self.save()
+
+    def __str__(self):
+        return self.schedule_id
 
 
 class Event(models.Model):
-    name= models.CharField(max_length=60)
-    day= models.PositiveIntegerField()
-    month= models.PositiveIntegerField()
+    name = models.CharField(max_length=60, primary_key=True)
+    day = models.PositiveIntegerField()
+    month = models.PositiveIntegerField()
     hour = models.PositiveIntegerField()
 
     def create(self):
@@ -52,19 +58,28 @@ class Event(models.Model):
         return self.name
 
 
-# class Tconstraints(models.Model):
-#     teacher= models.CharField(max_length=30, primary_key=True)
-#     day_of_week = models.PositiveIntegerField(primary_key=True)
-#     hour = models.PositiveIntegerField(primary_key=True)
-#     #priority= models.PositiveIntegerField(options=(0,1,2))
-#
-#     def create(self):
-#         self.save
+class Tconstraint(models.Model):
+    t_con_id = models.CharField(max_length=10, primary_key=True)
+    teacher = models.Foreignkey(User)
+    day_of_week = models.PositiveIntegerField()
+    hour = models.PositiveIntegerField()
+    priority = models.PositiveIntegerField()
+
+    def create(self):
+        self.save()
+
+    def __str__(self):
+        return self.t_con_id
 
 
-# class Tsubjects(models.Model):
-#     teacher = models.CharField(max_length=30, primary_key=True)
-#     subject = models.CharField(max_length=30, primary_key=True)
-#
-#     def create(self):
-#         self.save()
+class Tsubjects(models.Model):
+    t_sub_id = models.CharField(max_length=10, primary_key=True)
+    teacher = models.Foreignkey(User)
+    subject = models.CharField(max_length=30)
+
+    def create(self):
+        self.save()
+
+    def __str__(self):
+        return self.t_sub_id
+
